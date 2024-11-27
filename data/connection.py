835f88a443b5cd6cefca411c83d000255dd558c3
Th1nke <th1nkerldm@gmail.com>
2024-11-27 21:43:53 +0500

    database passwrd

--- data/connection.py
+++ data/connection.py
@@ -8,8 +8,8 @@ from datetime import datetime
 async def get_db_connection():
     conn = await psycopg.AsyncConnection.connect(
         user="postgres",
-        # password="14489",
-        password="0549",
+        password="14489",
+        # password="0549",
         host="localhost",
         port="5432",
         dbname="mydatabase"
@@ -24,8 +24,8 @@ async def get_db_connection():
 def get_db_connection_sync():
     conn = psycopg2.connect(
         user="postgres",
-        # password="14489",
-        password="0549",
+        password="14489",
+        # password="0549",
         host="localhost",
         port="5432",
         dbname="mydatabase"
